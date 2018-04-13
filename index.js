cd8c454f42845302a612368a88c95987acb8ba8c
ilmar <ilmar.tyrk@gmail.com>
2018-04-14 02:20:39 +0300

    fixed errors, added eslint

--- index.js
+++ index.js
@@ -9,23 +9,28 @@ var fs = require('fs');
 var fsExt = require('fs-extra');
 var Promise = require('bluebird');
 
+/**
+ * 
+ * @param {object} options settings for upload 
+ * @returns {object} Instance of StreamUpload
+ */
 function StreamUpload (options) {
-    var self = this;
+    var that = this;
 
-    self.settings = {
+    that.settings = {
         allowedExt: [],
         allowedTypes: [],
         maxSize: null,
         baseFolder: '',
         storage: {}
-    }
+    };
 
     var __parseExtensions = function () {
-        self.settings.allowedExt.forEach(function (ext) {
+        that.settings.allowedExt.forEach(function (ext) {
             var fileType = mime.lookup(ext);
-            self.settings.allowedTypes.push(fileType);
+            that.settings.allowedTypes.push(fileType);
         });
-        _.uniq(self.settings.allowedTypes);
+        _.uniq(that.settings.allowedTypes);
     };
 
     var __setExtensions = function (extensions) {
@@ -34,11 +39,11 @@ function StreamUpload (options) {
         }
 
         if (extensions.length) {
-            self.settings.allowedExt = extensions;
+            that.settings.allowedExt = extensions;
             __parseExtensions();
         }
 
-        return self.settings.allowedExt;
+        return that.settings.allowedExt;
     };
 
     var __setTypes = function (types) {
@@ -47,33 +52,36 @@ function StreamUpload (options) {
         }
 
         if (types.length) {
-            self.settings.allowedTypes = self.settings.allowedTypes.concat(types);
-            _.uniq(self.settings.allowedTypes);
+            that.settings.allowedTypes = that.settings.allowedTypes.concat(types);
+            _.uniq(that.settings.allowedTypes);
         }
 
-        return self.settings.allowedTypes;
+        return that.settings.allowedTypes;
     };
 
     var __setMaxSize = function (size) {
-        if (_.isNumber(size) &&  size > -1) {
-            self.settings.maxSize = size;
+        if (_.isNumber(size) && size > -1) {
+            that.settings.maxSize = size;
         }
-        return self.settings.maxSize;
+
+        return that.settings.maxSize;
     };
 
     var __setBaseFolder = function (folder) {
-        if(_.isString(folder)) {
-            self.settings.baseFolder = path.normalize(folder);
+        if (_.isString(folder)) {
+            that.settings.baseFolder = path.normalize(folder);
         }
-        return self.settings.baseFolder;
+
+        return that.settings.baseFolder;
     };
 
     var __setStorage = function (params) {
         if (_.isObject(params)) {
-            self.settings.storage = params;
+            that.settings.storage = params;
         }
-        return self.settings.storage;
-    }
+
+        return that.settings.storage;
+    };
 
     var __init = function (options) {
         if (options.extensions) {
@@ -92,20 +100,21 @@ function StreamUpload (options) {
     }
 
     var __checkFileType = function (type) {
-        if (self.settings.allowedTypes) {
-            for (var i=0; i < self.settings.allowedTypes.length; i++) {
-                var exp = new RegExp(self.settings.allowedTypes[i]);
+        if (that.settings.allowedTypes) {
+            for (var i = 0; i < that.settings.allowedTypes.length; i++) {
+                var exp = new RegExp(that.settings.allowedTypes[i]);
                 if (exp.test(type)) {
                     return true;
                 }
             }
+
             return false;
         }
     };
 
     var __checkFileSize = function (size) {
-        if (self.settings.maxSize) {
-            return size <= self.settings.maxSize;
+        if (that.settings.maxSize) {
+            return size <= that.settings.maxSize;
         }
 
         return true;
@@ -129,10 +138,10 @@ function StreamUpload (options) {
     };
 
     var __uploadToS3 = function (inputStream, params) {
-        var filename = params.filename || path.join(baseFolder, uuid.v4());
+        var filename = params.filename || path.join(that.settings.baseFolder, uuid.v4());
 
-        AWS.config.update({accessKeyId: self.settings.storage.accessKeyId, secretAccessKey: self.settings.storage.secretAccessKey, region: self.settings.storage.region});
-        var s3 = new AWS.S3({params: {Bucket: self.settings.storage.bucket}});
+        AWS.config.update({accessKeyId: that.settings.storage.accessKeyId, secretAccessKey: that.settings.storage.secretAccessKey, region: that.settings.storage.region});
+        var s3 = new AWS.S3({params: {Bucket: that.settings.storage.bucket}});
 
         return new Promise(function (resolve, reject) {
             return s3
@@ -144,13 +153,13 @@ function StreamUpload (options) {
                     return reject(err);
                 });
         });
-        
+
 
     };
 
-    var _uploadToLocal = function (inputStream, params) {
+    var __uploadToLocal = function (inputStream, params) {
         return new Promise(function (resolve, reject) {
-            var filename = params.filename || path.join(self.settings.baseFolder, uuid.v4());
+            var filename = params.filename || path.join(that.settings.baseFolder, uuid.v4());
             // Make sure the output directory is there.
             fsExt.ensureDirSync(path.dirname(filename));
 
@@ -169,10 +178,10 @@ function StreamUpload (options) {
     var __upload = function (stream, params) {
         var checkValid = __checkFile(params);
         if (checkValid === true) {
-            if (self.settings.storage.type && self.settings.storage.type.toLowerCase() === 's3') {
-                return _uploadToS3(stream, params);
+            if (that.settings.storage.type && that.settings.storage.type.toLowerCase() === 's3') {
+                return __uploadToS3(stream, params);
             } else {
-                return _uploadToLocal(stream, params);
+                return __uploadToLocal(stream, params);
             }
         } else {
             return new Promise(function (resolve, reject) {
@@ -180,9 +189,10 @@ function StreamUpload (options) {
             });
         }
     };
+
     return {
         upload: __upload,
-        settings: self.settings,
+        settings: that.settings,
         init: __init,
         setExtensions: __setExtensions,
         setTypes: __setTypes,
@@ -191,7 +201,7 @@ function StreamUpload (options) {
         checkFile: __checkFile,
         checkFileSize: __checkFileSize,
         checkFileType: __checkFileType
-    }
-};
+    };
+}
 
 module.exports = StreamUpload;
